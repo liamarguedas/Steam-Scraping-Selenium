b3838a492eda4c2adef8522eb25c5e2c45d2d90c
Liam Steven <iliamftw2013@gmail.com>
2023-04-10 15:03:59 -0300

    Simplified working code

--- scraper.py
+++ scraper.py
@@ -4,34 +4,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
+import numpy as np
 
-def GetSteamGames(ToScrape = 10):
+
+
+def PassAgeRestriction(Driver):
+    
+    # Select year 2000
+    Select(Driver.find_element(By.NAME, "ageYear")).select_by_value('2000')
+            
+    # Click submit
+    Driver.find_element(By.CLASS_NAME, "btnv6_blue_hoverfade.btn_medium").click()
+    
+    
+
+def GetSteamGames(ToScrape = 10, ToWait = 3):
+    
+    GamesName = list()
+    AgeRestriction = list()
 
     ChromeDriver = 'chromedriver.exe'
     driver = webdriver.Chrome(executable_path = ChromeDriver)
     
-    GamesName = list()
-    
     driver.get('https://store.steampowered.com/search/?ignore_preferences=1&filter=topsellers')
     
-    time.sleep(3)
+    time.sleep(ToWait)
+    
+    # Find all listed games
     GamesBanner = driver.find_elements(By.CLASS_NAME,"search_result_row.ds_collapse_flag.app_impression_tracked")
     
+    # For game in (Find all listed games)
     for item in GamesBanner:
         
-        try:
-            GameDriver = webdriver.Chrome(executable_path = ChromeDriver)
-            GameDriver.get(f"{item.get_attribute('href')}")
-            time.sleep(3)
-            GamesName.append(GameDriver.find_element(By.CLASS_NAME, "apphub_AppName").text)
+        # Get game URL to scrape
+        GameDriver = webdriver.Chrome(executable_path = ChromeDriver)
+        GameDriver.get(f"{item.get_attribute('href')}")
+        time.sleep(ToWait)
         
+        try: # Pass AgeRestriction if needed
+            PassAgeRestriction(Driver = GameDriver)
+            AgeRestriction.append(1)
         except:
-            
-            Select(GameDriver.find_element(By.NAME, "ageYear")).select_by_value('2000')
-            GameDriver.find_element(By.CLASS_NAME, "btnv6_blue_hoverfade.btn_medium").click()
-            time.sleep(3)
-            GamesName.append(GameDriver.find_element(By.CLASS_NAME, "apphub_AppName").text)
-            
+            AgeRestriction.append(0)
+          
+        time.sleep(ToWait)
+        
+        try: # Save game name
+            GamesName.append(GameDriver.find_element(By.CLASS_NAME, "apphub_AppName").text)   
+        except: # In case of an error, introduced an NaN value.
+            GamesName.append(np.nan)
+     
     return GamesName
         
 print(GetSteamGames())
